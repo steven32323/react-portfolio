1f513baf11f1b99b92e7f987320ba61d1254bff1
steven32323 <107620513+steven32323@users.noreply.github.com>
2023-04-19 09:15:12 +0100

    added description field to portfolios

--- src/info/Info.js
+++ src/info/Info.js
@@ -5,32 +5,17 @@ import mock3 from "../img/mock3.png";
 import mock4 from "../img/mock4.png";
 import mock5 from "../img/mock5.png";
 
-/* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
-how to make this template work best for you! */
-
 export let colors = ["rgb(0, 136, 255)", "rgb(111, 114, 133)"];
-/*
-I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
-These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
-the site.
- */
-
-/*
-So let's get started! Some of the info below is pretty self-explanatory, like 'firstName' and 'bio'. I'll try to explain anything
-that might not be obvious right off the bat :) I recommend looking at the template example live using "npm start" to get an idea
-of what each of the values mean.
- */
 
 export const info = {
   firstName: "Steven",
   lastName: "Smith",
-  initials: "ss", // the example uses first and last, but feel free to use three or more if you like.
+  initials: "ss",
   position: "a Frontend Developer",
-  selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
-  gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
+  selfPortrait: self,
+  gradient: `-webkit-linear-gradient(135deg, ${colors})`,
   baseColor: colors[0],
   miniBio: [
-    // these are just some "tidbits" about yourself. You can look at mine https://paytonjewell.github.io/#/ for an example if you'd like
     {
       emoji: "☕",
       text: "fueled by coffee (cliché but true)",
@@ -49,16 +34,6 @@ export const info = {
     },
   ],
   socials: [
-    // {
-    //   link: "https://facebook.com",
-    //   icon: "fa fa-facebook",
-    //   label: "facebook",
-    // },
-    // {
-    //   link: "https://instagram.com",
-    //   icon: "fa fa-instagram",
-    //   label: "instagram",
-    // },
     {
       link: "https://github.com/steven32323",
       icon: "fa fa-github",
@@ -69,13 +44,6 @@ export const info = {
       icon: "fa fa-linkedin",
       label: "linkedin",
     },
-    // {
-    //   link: "https://twitter.com",
-    //   icon: "fa fa-twitter",
-    //   label: "twitter",
-    // },
-    // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
-    // Just change the links so that they lead to your social profiles.
   ],
   bio: "Hello! I'm Steven. I'm currently a Senior Team Leader at Scotmid Co-operative. I'm a self-taught learner, I enjoy coding and gaming, and I'm both amazed and terrified of recent advances in AI.",
   skills: {
@@ -117,37 +85,44 @@ export const info = {
       label: "coding",
       emoji: "💻",
     },
-    // Same as above, change the emojis to match / relate to your hobbies or interests.
-    // You can also remove the emojis if you'd like, I just think they look cute :P
   ],
   portfolio: [
-    // This is where your portfolio projects will be detailed
     {
       title: "Tenzies",
+      description:
+        ' Made with React (and some heavy CSS to style the dice faces). A simple game with 10 dice, the player can click dice to "Hold" them, which prevent them from being rerolled. The goal is to have 10 matching dice.',
       live: "https://ssmith-tenzies.netlify.app/",
       source: "https://github.com/steven32323/Tenzies",
       image: mock5,
     },
     {
       title: "JustReact - a food ordering app",
+      description:
+        " Made with React. A food ordering app, which allows the customer to add a specified number of food items to their shopping cart. The customer can view their shopping cart and total at any time, and add/delete items",
       live: "https://justreact-foodapp.netlify.app/",
       source: "https://github.com/steven32323/react-FoodOrderApp",
       image: mock3,
     },
     {
       title: "Expense Tracker",
+      description:
+        " Made with React. More of a test app than anything else, allows the user to add expenses based on date, and amount. Allows filtering with the graph changing dynamically to show each months expenses proportional to the total expenses. Local Storage is used to store users data",
       live: "https://ssmithexpensetracker.netlify.app/",
       source: "https://github.com/steven32323/react-expense-tracker",
       image: mock1,
     },
     {
       title: "Bankist Landing Page",
+      description:
+        "Made using pure Javascript, practicing lazy loading, slow scrolling, modal windows and carousels.",
       live: "https://steven32323.github.io/Bankist-Landing-Page/",
       source: "https://github.com/steven32323/Bankist-Landing-Page",
       image: mock2,
     },
     {
       title: "Airbnb Experiences mockup",
+      description:
+        "Part of the Scrimba React course. A simple mockup of the Airbnb Experiences page. Top half of the page is static, with the bottom half dynamically rendering based on properties such as image, price, and availability",
       live: "https://airbnb-experiencesclone.netlify.app/",
       source: "https://github.com/steven32323/react-airbnb-clone",
       image: mock4,
